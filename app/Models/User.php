9cc7fa26eeb00577fed382c328203d29f4d6b0e4
Steven Martin W <steven.martin@mekari.com>
2022-05-14 20:43:07 +0700

    edit user table and fix import files

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'confirm_password', 'contact_number', 'gender', 'address', 'birthday', 'weight', 'height', 'description',
     ];
 
     /**
